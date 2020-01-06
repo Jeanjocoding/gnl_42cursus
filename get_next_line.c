5efd14f20e7871e0d20d180bd708c7335b88f5fd
Theophile LUCILLE <tlucille@e2r9p9.42.fr>
2020-01-06 12:55:57 +0100

    a priori ready to push

--- get_next_line.c
+++ get_next_line.c
@@ -1,109 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlucille <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/06 12:44:43 by tlucille          #+#    #+#             */
+/*   Updated: 2020/01/06 12:46:21 by tlucille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <stdio.h>
 
-size_t		gnl_strlen(const char *str)
+int		check_alloc(int fd, char *rest, char **rest_alloc)
 {
-	unsigned int	i;
+	char	buf[BUFFER_SIZE + 1];
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	if (fd < 0 || fd > MAX_FD || BUFFER_SIZE < 1 || read(fd, buf, 0) == -1)
+		return (-1);
+	if (rest != NULL && rest[0] != '\0')
+	{
+		if (!(*rest_alloc = gnl_strdup(rest)))
+			return (-1);
+	}
+	else
+		*rest_alloc = NULL;
+	return (0);
 }
 
 char	*gnl_extractor(char *str, char c)
 {
-	int	i;
-	int	j;
-	char 	*str2;
+	int		i;
+	int		j;
+	char	*str2;
 
 	i = 0;
 	j = 0;
-	//	printf("str in extractor : %s", str);
 	while (str[i] != '\0' && str[i] != c)
 	{
-		//	printf("%d", i);
 		i++;
 	}
-	if (!(str2 = gnl_strnew(i)))
+	if (!(str2 = (char*)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
 	while (j < i)
 	{
 		str2[j] = str[j];
 		j++;
 	}
+	str2[j] = '\0';
 	return (str2);
 }
 
-int	reader(int fd, char **line, char **rest)
+int		reader_ret(char **line, char **rest, char **str, char *str2)
 {
-	int	ret;
+	if (str2 != NULL)
+	{
+		if (!(*line = gnl_extractor(*str, '\n')))
+			return (-1);
+		if (!(*rest = gnl_strdup(str2)))
+			return (-1);
+	}
+	else
+	{
+		if (!(*line = gnl_strdup(*str)))
+			return (-1);
+	}
+	free(*str);
+	*str = NULL;
+	return (1);
+}
+
+int		reader(int fd, char **line, char **rest)
+{
+	int		ret;
 	char	*str;
 	char	buf[BUFFER_SIZE + 1];
 	char	*str2;
 
 	gnl_memset(buf, '\0', BUFFER_SIZE + 1);
 	ret = read(fd, buf, BUFFER_SIZE);
-	//	printf(" rest : %s, ret : %d\n", *rest, ret);
 	if ((*rest == NULL || *rest[0] == '\0') && (ret == 0 || ret == -1))
 		return (ret);
-//	printf("rest : %s\n", *rest);
-	//	printf("buf : %s\n", buf);
 	if (!(str = gnl_strjoin(rest, buf, 1)))
 		return (-1);
-//	free(*rest);
-//	*rest = NULL;
-	//	printf("str : %s\n", str);
 	while (ret && gnl_strchr(str, '\n') == NULL)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
 		if (!(str = gnl_strjoin(&str, buf, 1)))
 			return (-1);
 	}
-	//	printf("str round 2 : %s\n", str);
 	str2 = gnl_strchr(str, '\n');
-	if (str2 != NULL)
-	{
-		if (!(*line = gnl_extractor(str, '\n')))
-			return (-1);
-		//		printf("str2 in reader : %s", str2);
-		if (!(*rest = gnl_strdup(str2)))
-			return (-1);
-	}
-	else
-	{
-		if (!(*line = gnl_strdup(str)))
-			return (-1);
-	}
-	free(str);
-	str = NULL;
-
-	return (1);
+	return (reader_ret(line, rest, &str, str2));
 }
 
-int	get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
 	static char	rest[MAX_FD][BUFFER_SIZE + 1];
 	char		*rest_alloc;
-	char		buf[BUFFER_SIZE + 1];
-	int		ret;
-	int		i;
+	int			ret;
+	int			i;
 
 	i = -1;
-	if (fd < 0 || fd > MAX_FD || BUFFER_SIZE < 1 || line
-			== NULL || read(fd, buf, 0) == -1)
+	if (check_alloc(fd, (char*)rest[fd], &rest_alloc) == -1 || line == NULL)
 		return (-1);
-	if (rest[fd] != NULL && rest[fd][0] != '\0')
-	{
-		if (!(rest_alloc = gnl_strdup((const char *)rest[fd])))
-			return (-1);
-	}
-	else
-		rest_alloc = NULL;
-//	printf("rest_alloc : %s\n", rest_alloc);
 	ret = reader(fd, line, &rest_alloc);
-	//	printf("rest_alloc : %s\n", rest_alloc);
-	//	printf("ret : %d\n", ret);
 	if (rest_alloc != NULL)
 	{
 		while (rest_alloc[++i])
@@ -114,6 +114,5 @@ int	get_next_line(int fd, char **line)
 	}
 	else
 		rest[fd][0] = '\0';
-	//	printf("rest_fd : %s\n", rest[fd]);
 	return (ret);
 }
